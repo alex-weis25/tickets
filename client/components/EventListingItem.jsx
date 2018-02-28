fdcd4c2d236a44b04f87636b728bfec0391b437e
Matthew Appleby <mappleby1@me.com>
2018-02-27 19:58:34 -0500

    modified allevents component to have new attributes and styling, plus fixed navbar

--- client/components/EventListingItem.jsx
+++ client/components/EventListingItem.jsx
@@ -8,25 +8,26 @@ import { Link } from 'react-router-dom'
 
 export default function EventListingItem(props) {
   const {event} = props
-
   return (
-    <div>
+    <div className="eventListing">
       <Link to={`event/${event.id}`}>
+        <img src={event.imgUrl} />
+        <div className="eventInfo">
+          <div className="eventNameVenue">
+            <h2>{event.name}</h2>
+            <h4> at the </h4>
+            <h2>{event.venue.name}</h2>
+          </div>
 
-        <h2>
-          {event.name}
-        </h2>
-        <h2>
-          {event.venue.name}
-        </h2>
-        {
-          event.tickets.length ?
+          {
+            event.tickets.length ?
 
-          (<h2>
-            {event.tickets.length} Tickets Remaining
-          </h2>) :
-          ( <h2> SOLD OUT! </h2>)
-        }
+            (<div className="eventTickets">
+              {event.tickets.length} Tickets Remaining
+            </div>) :
+            ( <div className="SoldOut"> SOLD OUT! </div>)
+          }
+        </div>
       </Link>
     </div>
 
