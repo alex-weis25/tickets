ca0c720270177f69e0e371b0ac5fe249b141ea44
Alex Weis <alex.weis25@gmail.com>
2018-03-02 12:09:51 -0500

    adding cart api route

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'))
 router.use('/events', require('./events'));
 router.use('/venues', require('./venues'))
 router.use('/orders', require('./orders'))
+router.use('/cart', require('./cart'))
 // router.use('/orderlines', require('./orderlines'))
 
 router.use((req, res, next) => {
