d1acedb8496a21bd90493094504cf0f2a8961fbe
Alex Weis <alex.weis25@gmail.com>
2018-03-03 13:35:42 -0500

    updating for saturday afternoon

--- client/app.js
+++ client/app.js
@@ -3,20 +3,15 @@ import SquareConnect from 'square-connect'
 import {Navbar} from './components'
 import Routes from './routes'
 
+
 const defaultClient = SquareConnect.ApiClient.instance
-var oauth2 = defaultClient.authentications['oauth2'];
-oauth2.accessToken = process.env.SQUARE_SANDBOX_TOKEN;
+const oauth2 = defaultClient.authentications['oauth2'];
+const listLocations = SquareConnect.ListLocationsRequest;
+const api = new SquareConnect.LocationsApi();
 
-var api = new SquareConnect.LocationsApi();
-
-api.listLocations(function(data) {
-  console.log('API called successfully. Returned data: ' + data);
-}, function(error) {
-  console.error(error);
-});
 
 const App = () => {
-  console.log("Square", SquareConnect, defaultClient);
+  console.log("Square", SquareConnect, listLocations);
   return (
     <div>
       <Navbar />
--- server/payments/sqaure.js
+++ server/payments/sqaure.js
@@ -1,1 +1,12 @@
+'use strict'
 
+const SquareConnect = require('square-connect');
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+const defaultClient = SquareConnect.ApiClient.instance
+const oauth2 = defaultClient.authentications['oauth2'];
+const api = new SquareConnect.LocationsApi();
+
+console.log('SquareConnect & DefaultClient', SquareConnect, defaultClient);
