d3592e6833f31288aab8b2f5ca355828cf8e56b9
Matthew Appleby <mappleby1@me.com>
2018-03-05 19:02:18 -0500

    got rid of some console logs

--- server/api/creditCard.js
+++ server/api/creditCard.js
@@ -6,10 +6,6 @@ var stripe = require('stripe')(process.env.STRIPE_CLIENT_SECRET);
 //Credit Card Authorization
 router.post('/', (req, res, next) => {
   const token = req.body.response.id;
-  console.log('cartTotal: ', req.body.cartTotal);
-  console.log('typeof cartTotal: ', typeof req.body.cartTotal);
-  console.log('orderId: ', req.body.orderId);
-  console.log('user: ', req.body.user);
   stripe.charges.create({
     amount: req.body.cartTotal * 100,
     currency: "usd",
